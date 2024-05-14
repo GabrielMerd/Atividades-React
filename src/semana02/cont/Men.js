aec89f493c05c4764e2abad78f2c7646c8a39790
Gabriel Meredija <114880504+GabrielMerd@users.noreply.github.com>
2024-05-14 08:36:20 -0300

    Add files via upload

---
+++ src/semana02/cont/Men.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Buttons from "./ButtonCount";
+import imgMan from "../../img/garoto.jpg";
+import "./style.css";
+
+export default function Men({ count, setCount }) 
+{
+  return (
+    <div className="counter">
+      <div className="div">
+        <h3>Homens</h3>
+        <img className="imgPerson" src={imgMan} alt="Man" />
+        <div>
+          <Buttons count={count} setCount={setCount} />
+        </div>
+      </div>
+    </div>
+  );
+}
