a9fac320afecb01773a27a0131bee009bfa64da8
Gabriel Meredija <114880504+GabrielMerd@users.noreply.github.com>
2024-05-21 13:26:51 -0300

    Add files via upload

---
+++ src/semana03/Atividade03.js
@@ -0,0 +1,29 @@
+import FirstComponent from './FirstComponent';
+import Profile from './Profile';
+import Gallery from './Gallery';
+import { Link } from "react-router-dom";
+import TodoList from './TodoList';
+import TodoListPerson from './TodoListPerson';
+import ProfileCard from './Card';
+import PackingList from './PackingList';
+import List from './List';
+import TeaGathering from './Cup';
+
+export default function Atividade03()
+{
+    return (
+        <>
+          <Link to="/"> Voltar para Homepage </Link>
+          <h1> Atividade 03 </h1>
+          <FirstComponent />
+          <Profile />
+          <Gallery />
+          <TodoList />
+          <TodoListPerson />
+          <ProfileCard />
+          <PackingList />
+          <List />
+          <TeaGathering />
+        </>
+    );
+}
