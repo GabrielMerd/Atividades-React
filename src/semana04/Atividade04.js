3d727cbcfce797514538610c7d7a75f83d726cdc
Gabriel Meredija <114880504+GabrielMerd@users.noreply.github.com>
2024-05-28 08:58:37 -0300

    Add files via upload

---
+++ src/semana04/Atividade04.js
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import App from "./App";
+import Gallery from "./Gallery";
+import Form from "./Form";
+import MovingDot from "./MovingDot";
+import Form2 from "./Form2";
+import Form3 from "./Form3";
+import Counter from "./Counter";
+import List from "./List";
+import List2 from "./List2";
+import ShapeEditor from "./ShapeEditor";
+import CounterList from "./CounterList";
+import List3 from "./List3";
+import List4 from "./List4";
+import BucketList from "./BucketList";
+
+export function Atividade04() 
+{
+    return (
+        <>
+            <div>
+                <Link to="/" style={{textDecoration: "none", color: "black"}}>
+                    <button type="button" class="btn btn-dark btn"> 
+                        Voltar  
+                    </button>  
+                
+                </Link>
+            </div>
+            <App />
+            <Gallery />
+            <Counter />
+            <Form />
+            <MovingDot />
+            <Form2 />
+            <Form3 />
+            <List />
+            <List2 />
+            <ShapeEditor />
+            <CounterList />
+            <List3 />
+            <List4 />
+            <BucketList />
+        </>
+    );
+}
---
+++ src/semana04/Home.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => 
+  {
+  return (
+    <div className="text-center" style={{padding: "0.5%"}}>
+      <div id="home-container" className="cont">
+        <nav>
+          <div style={{padding: "0.5%"}}>
+            <Link to="/Atividade01" style={{textDecoration: "none", color: "black"}}> 
+              <button type="button" style={{width: "80%"}} class="btn btn-dark btn-lg"> 
+                    Atividade 01 
+              </button> 
+            </Link>
+          </div>
+          
+          <div style={{padding: "0.5%"}}>
+            <Link to="/Atividade02" style={{textDecoration: "none", color: "black"}}> 
+              <button type="button" style={{width: "80%"}} class="btn btn-dark btn-lg"> 
+                    Atividade 02 
+              </button> 
+            </Link>
+          </div>
+          
+          <div style={{padding: "0.5%"}}>
+            <Link to="/Atividade03" style={{textDecoration: "none", color: "black"}}> 
+              <button type="button" style={{width: "80%"}} class="btn btn-dark btn-lg"> 
+                    Atividade 03 
+              </button> 
+            </Link>
+          </div>
+          
+          <div style={{padding: "0.5%"}}>
+            <Link to="/Atividade04" style={{textDecoration: "none", color: "black"}}> 
+              <button type="button" style={{width: "80%"}} class="btn btn-dark btn-lg"> 
+                    Atividade 04 
+              </button> 
+            </Link>
+          </div>
+          
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
